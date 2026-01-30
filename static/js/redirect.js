1771ce04ee8723eb715e61574410aeea1a8581de
BrulesMC <105064807+BrulesMC@users.noreply.github.com>
2026-01-30 10:09:29 -0500

    Added my base

---
+++ static/js/redirect.js
@@ -0,0 +1,9 @@
+function redirectLoginPage(){
+    window.location.href = "/login";
+}
+function redirectRegisterPage(){
+    window.location.href = "/register";
+}
+function redirectHomePage(){
+    window.location.href = "/";
+}
