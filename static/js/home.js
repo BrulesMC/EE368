f9d4b571513b7f9623e14d04b9183048554a54bc
Brendan <105064807+BrulesMC@users.noreply.github.com>
2026-04-13 17:30:52 -0400

    Commented home.js

--- static/js/home.js
+++ static/js/home.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', async () => {
-
+  //get current use
   const res = await fetch('/api/me', {
     credentials: 'include'
   });
-
+  //if we did not get a user they are not logged in, redirect to login
   if (!res.ok) {
     window.location.href = '/';
     return;
   }
-
+  //if we are good get user data
   const user = await res.json();
-
+  //username
   const fullName = user.username || user.email;
   document.getElementById('welcome').textContent =
     'Hello ' + fullName + ', you have logged in';
-
+  //no reset button if github user
   if (user.type === "github") {
     document.getElementById('resetBtn').style.display = "none";
   }
-
+  //setup the logout button
   document.getElementById('logoutBtn').addEventListener('click', async () => {
     await fetch('/api/logout', {
       method: 'POST',
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
     window.location.href = '/';
   });
-
+  //setup reset button if is still there
   document.getElementById('resetBtn').addEventListener('click', () => {
     window.location.href = '/reset';
   });
